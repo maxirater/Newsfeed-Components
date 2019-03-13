dab85ff7a65921bf9177eee84618bdf134816d99
Brad Cantrell <brad777mx@hotmail.com>
2019-03-13 14:02:44 -0700

    mostly done, not completely working

--- Article/Article.js
+++ Article/Article.js
@@ -1,21 +1,24 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    constructor( domElement ) {
+        // assign this.domElement to the passed in domElement
+        this.domElement = domElement;
+        // create a reference to the ".expandButton" class. 
+        this.expandButton = document.querySelector( '.expandButton' );
+        // Using your expandButton reference, update the text on your expandButton to say "expand"
+        this.expandButton.textContent = 'expand';
+        // Set a click handler on the expandButton reference, calling the expandArticle method.
+        this.expandButton.addEventListener( 'click', this.expandArticle );
+    }
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
-  }
+    // expandArticle() {
+    //     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    //     this.domElement.classList.toggle( 'article-open' );
+    // }
+    expandArticle = () => {
+        this.domElement.classList.toggle( 'article-open' );
+    }
 }
 
 /* START HERE: 
@@ -27,3 +30,7 @@ class Article {
 */
 
 let articles;
+articles = document.querySelectorAll( '.article' );
+articles.forEach( x => {
+    x = new Article( x );
+} )
